547303075147aa883fd45e2bba2b26dd3b493cea
fredp03 <128590812+fredp03@users.noreply.github.com>
2025-09-01 17:42:55 -0400

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to New UI V2</h1>
+        <p>
+          This is an empty React app ready for development.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
